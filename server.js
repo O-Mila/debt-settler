de5f3c1851fd3a9465f303b00d3f65313f389094
Oriol <oriolmilajansa@gmail.com>
2019-03-21 10:10:39 +0100

    Explicit mlab link

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ require("dotenv").config()
 
 const secret = process.env.SECRET || "The Speech of the Stars",
 PORT 		 = process.env.PORT || 8080,
-url 		 = process.env.DATABASEURL || "mongodb://localhost/debt-settler";
-
+//url 		 = process.env.DATABASEURL || "mongodb://localhost/debt-settler";
+url          = "mongodb://oriolmilajansa:0112omj8986@ds341605.mlab.com:41605/debt-settler"
 // Connect database
 mongoose.connect(url, { useNewUrlParser: true });
 
